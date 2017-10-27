16b3ee51b7d0241e9de0783212dce3c53f525422
Charles Reid <charlesreid1@gmail.com>
2017-10-26 23:06:13 -0700

    expand get_data to include more .xml files

--- get_data.sh
+++ get_data.sh
@@ -6,6 +6,16 @@ wget -O face.jpeg https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Geor
 # Haar face detection parameters
 wget https://raw.githubusercontent.com/opencv/opencv/2.4/data/haarcascades/haarcascade_frontalface_alt.xml
 wget https://raw.githubusercontent.com/opencv/opencv/2.4/data/haarcascades/haarcascade_frontalface_alt2.xml
-wget https://raw.githubusercontent.com/opencv/opencv/2.4/data/haarcascades/haarcascade_eye.xml
 
+# Haar eye detection parameters
+wget https://raw.githubusercontent.com/opencv/opencv/2.4/data/haarcascades/haarcascade_eye.xml
+https://raw.githubusercontent.com/opencv/opencv/2.4/data/haarcascades/haarcascade_righteye_2splits.xml
+https://raw.githubusercontent.com/opencv/opencv/2.4/data/haarcascades/haarcascade_lefteye_2splits.xml
+https://raw.githubusercontent.com/opencv/opencv/2.4/data/haarcascades/haarcascade_mcs_righteye.xml
+https://raw.githubusercontent.com/opencv/opencv/2.4/data/haarcascades/haarcascade_mcs_lefteye.xml
+
+# These two are different
+# wget https://raw.githubusercontent.com/opencv/opencv/2.4/data/haarcascades/haarcascade_mcs_eyepair_big.xml
+# This one works better (I think?):
+wget https://raw.githubusercontent.com/opencv/opencv_contrib/master/modules/face/data/cascades/haarcascade_mcs_eyepair_big.xml
 

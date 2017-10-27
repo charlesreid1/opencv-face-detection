c636a4e8c3a9273d4c4be8338e95aee689790108
Charles Reid <charlesreid1@gmail.com>
2017-10-26 23:01:27 -0700

    Extensive updates to find face in image script.

--- README.md
+++ README.md
@@ -1,7 +1,62 @@
 # OpenCV Face Detection
 
-This repository contains utility scripts for 
-doing facial detection in images using OpenCV.
+This repository contains utility scripts for doing facial detection in images using OpenCV.
+
+Most of these scripts are experiments. The goal is to incorporate these scripts
+into scripts for a Raspberry Pi facial detection device - see the [pi-opencv](https://github.com/charlesreid1-raspberry-pi/pi-opencv) 
+repository.
+
+## Repository Organization
+
+Completed tasks:
+* <s>Take a photo using a webcam</s>
+
+In-progress tasks:
+* Find a face in a photograph (using test images from internet)
+* Find a face in a webcam photograph (using webcam photos)
+* Find eyes in a photograph (using test images from internet)
+* Find eyes in a webcam photograph (using webcam photos)
+* Find a face that may be rotated up to N degrees (using webcam)
+* Detect sideways faces using eyes angle
+
+## Take a Photo Using Webcam
+
+`take_photo.py`
+* Imports OpenCV
+* Captures an image from the specified camera device
+* Transposes the image (rotate right)
+* Saves the images to a file
+* Uses imshow to show the images on screen
+* (Future work) image is a numpy array, so do further processing/exports
+
+## Find a Face
+
+`find_face_image.py`
+* Opens an image on disk using OpenCV or Pillow
+* Create cascade classifier to find faces
+* Get rectangles containing faces
+* Draw rectangles around faces
+* Get rectangles containing eyes
+* Draw rectangles around eyes
+* Show the image of the face with rectangles
+
+Notes:
+* It's easy to accomplish 90% of the task, but it's a pain in the ass to get it working consistently.
+* Each different photo of a face requires different `scaleFactor` and `minNeighbors` settings
+* Eyes are also difficult to detect without fiddling with settings that are specific to each image.
+* [This](https://stackoverflow.com/questions/16128637/opencv-haarlike-eye-detection#16131846) SO question mentions an eye pair Haar cascade file.
+* [opencv-contrib](https://github.com/opencv/opencv_contrib) repo has additional cascade .xml files
+* See [opencv-contrib modules/face/data/cascades](https://github.com/opencv/opencv_contrib/tree/master/modules/face/data/cascades)
+
+## Find Face in Webcam Photograph
+
+## Find Rotated Face
+
+## Detect 
+
+
+
+
 
 ## Find a Face
 
--- find_face_image.py
+++ find_face_image.py
@@ -13,21 +13,165 @@ Basic workflow:
     * Draw the picture and draw bounding boxes around faces and eyes
 """
 
-def jpeg_to_np(filename):
+def main():
+    
+    face_settings = {
+            'scaleFactor'   : 1.3,
+            'minNeighbors'  : 1,
+            'minSize'       : (5,5),
+            'flags'         :  cv2.cv.CV_HAAR_SCALE_IMAGE|cv2.cv.CV_HAAR_DO_ROUGH_SEARCH
+    }
+    
+    eye_settings = {
+            'scaleFactor'   : 1.3,
+            'minNeighbors'  : 3,
+            'minSize'       : (5,5),
+            'flags'         :  cv2.cv.CV_HAAR_SCALE_IMAGE
+    }
+
+    # Other flags: 
+    # cv2.cv.CV_HAAR_SCALE_IMAGE
+    # cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT
+    # cv2.cv.CV_HAAR_DO_ROUGH_SEARCH
+
+
+
+    ## Obama
+    #IMAGE_FILE                    = "images/obama.jpg"
+    #face_settings['scaleFactor']  = 1.3
+    #face_settings['minNeighbors'] = 1
+    #eye_settings['scaleFactor']   = 1.3
+    #eye_settings['minNeighbors']  = 3
+    
+    ## GWBush
+    #IMAGE_FILE                    = "images/bush.jpg"
+    #face_settings['scaleFactor']  = 1.3
+    #face_settings['minNeighbors'] = 1
+    #eye_settings['scaleFactor']   = 1.3
+    #eye_settings['minNeighbors']  = 3
+    
+    ## Me
+    #IMAGE_FILE                    = "images/me.jpg"
+    #face_settings['scaleFactor']  = 1.3
+    #face_settings['minNeighbors'] = 1
+    #eye_settings['scaleFactor']   = 1.3
+    #eye_settings['minNeighbors']  = 3
+    
+    # Me too
+    IMAGE_FILE                    = "images/opencvT.png"
+    face_settings['scaleFactor']  = 1.3
+    face_settings['minNeighbors'] = 1
+    eye_settings['scaleFactor']   = 1.3
+    eye_settings['minNeighbors']  = 3
+    
+    draw_face_boxes(IMAGE_FILE, face_settings, eye_settings)
+
+
+
+def draw_face_boxes(filename, face_settings, eye_settings):
+
+    image = img2np_opencv(filename)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+
+    face_classifier  = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+    #eye_classifier   = cv2.CascadeClassifier("haarcascade_eye.xml")
+    eye_classifier   = cv2.CascadeClassifier("haarcascade_mcs_eyepair_big.xml")
+    left_classifier  = cv2.CascadeClassifier("haarcascade_lefteye_2splits.xml")
+    right_classifier = cv2.CascadeClassifier("haarcascade_righteye_2splits.xml")
+
+    # --------
+    # FACES:
+
+    # Get rectangle for entire face
+    print("From faces:")
+    rects = detect(gray, face_classifier, face_settings)
+
+    # Draw face rectangles
+    vis = image.copy()
+    draw_rects(vis, rects, (255, 0, 0))
+
+    # --------
+    # EYES:
+
+    ### # Get eye subrectangle
+    ### print("From outside:")
+    ### subrects = detect(gray,    eye_classifier, eye_settings)
+    ### ###subrectsL = detect(gray,  left_classifier, eye_settings)
+    ### ###subrectsR = detect(gray, right_classifier, eye_settings)
+
+    ### print("Do we have rectangles?")
+    ### print(subrects)
+    ### # Draw eye subrectangle
+    ### draw_rects(vis, subrects, (255, 0, 0))
+    ### #draw_rects(vis, subrectsL, (40, 40, 255))
+    ### #draw_rects(vis, subrectsR, (40, 40, 255))
+
+    for i, (x1, y1, x2, y2) in enumerate(rects):
+        # Crop to just this face
+        grayface = gray[y1:y2, x1:x2]
+        visface  = vis[y1:y2,  x1:x2]
+
+        # Get rectangle for eye
+        print("From inside:")
+
+        #subrects  = detect(grayface,   eye_classifier, eye_settings)
+        subrects  = detect(grayface,   eye_classifier, {})#, eye_settings)
+        draw_rects(visface, subrects, (50,50,255))
+
+        #subrectsL = detect(grayface,  left_classifier, eye_settings)
+        #subrectsR = detect(grayface, right_classifier, eye_settings)
+
+        #draw_rects(visface, subrectsL, (100, 100, 255))
+        #draw_rects(visface, subrectsR, (100, 255, 100))
+
+
+    cv2.imwrite("detected_faces.png", vis)
+    #cv2.imshow('facedetect', vis)
+    #cv2.waitKey()
+
+
+
+def detect(img, cascade, settings):
     """
-    Loads jpeg image using Pillow,
-    converts to a 3D Numpy array
-    with shape (width, height, channels)
+    Given an image and a cascade object,
+    detect features.
+    """
+    # Parameters taken verbatim from OpenCV 2.4 example:
+    # https://github.com/opencv/opencv/blob/2.4/samples/python2/facedetect.py
+    rects = cascade.detectMultiScale(img, 
+                        **settings)
+
+    if len(rects) == 0:
+        return []
+
+    print(rects)
+
+    # Turn width and height of box
+    # into plot-ready image coordinates.
+    # rects contains (x,y),(w,h), return (x,y),(x+w,x+h)
+
+    rects[:,2:] += rects[:,:2]
+    return rects
+
+
+
+def img2np_pillow(filename):
+    """
+    Load an image using Pillow 
+    and convert it to a 3D Numpy array
+    with shape (W, H, 3)
     """
     with Image.open(filename) as image:
         nparr = np.fromstring(image.tobytes(), dtype=np.uint8)
         nparr = im_arr.reshape((image.size[1], image.size[0], 3))
     return nparr 
 
-def jpeg_to_np2(filename):
+def img2np_opencv(filename):
     """
-    Loads jpeg image as a Numpy array
-    using OpenCV
+    Load an image using OpenCV
+    and convert it to a 3D Numpy array
+    with shape (W, H, 3)
     """
     img = cv2.imread(filename)
     return img
@@ -40,64 +184,6 @@ def draw_rects(img, rects, color):
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
-def detect(img, cascade):
-    """
-    Given an image and a cascade object,
-    detect features.
-    """
-    # Parameters taken verbatim from OpenCV 2.4 example:
-    # https://github.com/opencv/opencv/blob/2.4/samples/python2/facedetect.py
-    rects = cascade.detectMultiScale(gray, 
-                        scaleFactor=1.3, 
-                        minNeighbors=1, 
-                        minSize=(30, 30), 
-                        flags = cv.CV_HAAR_SCALE_IMAGE)
-
-    if len(rects) == 0:
-        return []
-
-    print(rects)
-    # Turn width and height of box
-    # into plot-ready image coordinates.
-    # rects contains (x,y),(w,h), return (x,y),(x+w,x+h)
-    rects[:,2:] += rects[:,:2]
-    return rects
-
 if __name__=="__main__":
-    filename = "face.jpeg"
-    image = jpeg_to_np2(filename)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
-
-    cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
-    nested = cv2.CascadeClassifier("haarcascade_eye.xml")
-
-    # Get rectangle for entire face
-    print("From faces:")
-    rects = detect(gray, cascade)
-
-    # Draw face rectangles
-    vis = image.copy()
-    draw_rects(vis, rects, (0,255,0))
-
-    # Get eye subrectangle
-    print("From outside:")
-    subrects = detect(gray, nested)
-
-    # Draw eye subrectangle
-    draw_rects(vis, subrects, (255, 0, 0))
-
-    for i, (x1, y1, x2, y2) in enumerate(rects):
-        # Crop to just this face
-        grayeye = gray[y1:y2, x1:x2]
-        viseye  = vis[y1:y2,  x1:x2]
-
-        # Get rectangle for eye
-        print("From inside:")
-        subrects = detect(grayeye.copy(), nested)
-        #draw_rects(viseye, subrects, (100,100,255))
-        draw_rects(vis, subrects, (100,100,255))
-
-    cv2.imshow('facedetect', vis)
-    cv2.waitKey()
+    main()
 

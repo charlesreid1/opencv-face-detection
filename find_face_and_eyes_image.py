feab431440966759e566da06a4f1e0307885cc8a
Charles Reid <charlesreid1@gmail.com>
2017-11-07 12:56:17 -0800

    look for left and right eyes.

--- find_face_and_eyes_image.py
+++ find_face_and_eyes_image.py
@@ -26,7 +26,7 @@ def main():
             'scaleFactor'   : 1.3,
             'minNeighbors'  : 3,
             'minSize'       : (5,5),
-            'flags'         :  cv2.cv.CV_HAAR_SCALE_IMAGE
+            'flags'         :  cv2.cv.CV_HAAR_SCALE_IMAGE|cv2.cv.CV_HAAR_DO_ROUGH_SEARCH
     }
 
     # Other flags: 
@@ -36,33 +36,33 @@ def main():
 
 
 
-    ## Obama - works
+    ## Obama - works (but detects to many features)
     #IMAGE_FILE                    = "images/obama.jpg"
     #face_settings['scaleFactor']  = 1.3
     #face_settings['minNeighbors'] = 1
     #eye_settings['scaleFactor']   = 1.3
     #eye_settings['minNeighbors']  = 3
     
-    # GWBush - works
-    IMAGE_FILE                    = "images/bush.jpg"
-    face_settings['scaleFactor']  = 1.3
-    face_settings['minNeighbors'] = 1
-    eye_settings['scaleFactor']   = 1.3
-    eye_settings['minNeighbors']  = 3
+    ## GWBush - works
+    #IMAGE_FILE                    = "images/bush.jpg"
+    #face_settings['scaleFactor']  = 1.3
+    #face_settings['minNeighbors'] = 1
+    #eye_settings['scaleFactor']   = 1.3
+    #eye_settings['minNeighbors']  = 3
     
-    ## Me - works
+    ## Me - works (but four total features, with overlap)
     #IMAGE_FILE                    = "images/me.jpg"
     #face_settings['scaleFactor']  = 1.3
     #face_settings['minNeighbors'] = 1
     #eye_settings['scaleFactor']   = 1.3
     #eye_settings['minNeighbors']  = 3
     
-    ## Me too - works
-    #IMAGE_FILE                    = "images/opencvT.png"
-    #face_settings['scaleFactor']  = 1.3
-    #face_settings['minNeighbors'] = 1
-    #eye_settings['scaleFactor']   = 1.3
-    #eye_settings['minNeighbors']  = 3
+    # Photo from webcam
+    IMAGE_FILE                    = "images/opencv.png"
+    face_settings['scaleFactor']  = 1.3
+    face_settings['minNeighbors'] = 1
+    eye_settings['scaleFactor']   = 1.3
+    eye_settings['minNeighbors']  = 3
     
     draw_face_boxes(IMAGE_FILE, face_settings, eye_settings)
 
@@ -74,18 +74,21 @@ def draw_face_boxes(filename, face_settings, eye_settings):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
 
-    face_classifier  = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
-    #eye_classifier   = cv2.CascadeClassifier("haarcascade_eye.xml")
-    eye_classifier   = cv2.CascadeClassifier("haarcascade_mcs_eyepair_big.xml")
-    left_classifier  = cv2.CascadeClassifier("haarcascade_lefteye_2splits.xml")
-    right_classifier = cv2.CascadeClassifier("haarcascade_righteye_2splits.xml")
+    face_classifier0    = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    face_classifier1    = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+    face_classifier2    = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+
+    eyepair_classifier  = cv2.CascadeClassifier("haarcascade_mcs_eyepair_big.xml")
+    left_classifier     = cv2.CascadeClassifier("haarcascade_lefteye_2splits.xml")
+    right_classifier    = cv2.CascadeClassifier("haarcascade_righteye_2splits.xml")
+    eye_classifier      = cv2.CascadeClassifier("haarcascade_eye.xml")
 
     # --------
     # FACES:
 
     # Get rectangle for entire face
     print("From faces:")
-    rects = detect(gray, face_classifier, face_settings)
+    rects = detect(gray, face_classifier2, face_settings)
 
     # Draw face rectangles
     vis = image.copy()
@@ -103,20 +106,17 @@ def draw_face_boxes(filename, face_settings, eye_settings):
         # Get rectangle for eye
         print("From inside:")
 
-        #subrects  = detect(grayface,   eye_classifier, eye_settings)
-        subrects  = detect(grayface,   eye_classifier, {})#, eye_settings)
-        draw_rects(visface, subrects, (50,50,255))
-
-        #subrectsL = detect(grayface,  left_classifier, eye_settings)
-        #subrectsR = detect(grayface, right_classifier, eye_settings)
-
-        #draw_rects(visface, subrectsL, (100, 100, 255))
-        #draw_rects(visface, subrectsR, (100, 255, 100))
+        all_classifiers = [left_classifier, right_classifier]
+        all_colors = [(50,255,50), (50,255,50)]
+        for classifier,color in zip(all_classifiers, all_colors):
+            subrects = detect(grayface, classifier, eye_settings)
+            draw_rects(visface, subrects, color)
 
 
-    cv2.imwrite("images/detected_faces2.jpg", vis)
-    #cv2.imshow('facedetect', vis)
-    #cv2.waitKey()
+    cv2.imwrite("images/detected_faces_eyes.jpg", vis)
+
+    cv2.imshow('facedetect', vis)
+    cv2.waitKey()
 
 
 

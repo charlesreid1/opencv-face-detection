0195b0d044f75ff75a2aea8efc7eed316ff4a80e
Charles Reid <charlesreid1@gmail.com>
2017-11-07 11:57:19 -0800

    rename output files.

--- basic_face_detection.py
+++ basic_face_detection.py
@@ -20,7 +20,7 @@ for (x,y,w,h) in faces:
     for (ex,ey,ew,eh) in eyes:
         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
 
-cv2.imwrite("images/detected_faces.jpg", img)
+cv2.imwrite("images/detected_faces_basic.jpg", img)
 cv2.imshow('img',img)
 cv2.waitKey(0)
 
